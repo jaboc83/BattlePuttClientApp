7d17c58636bc5f4c3e4c8786b08eb1bef3e16250
Jake Moening <jaboc83@gmail.com>
2022-12-15 10:44:19 -0600

    Get it running

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,10 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { createTheme, ThemeProvider } from '@mui/material';
+import { Box, createTheme, ThemeProvider } from '@mui/material';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import Landing from './pages/Landing';
-import Battle from './pages/Battle';
 import { themeOptions } from './theme';
-import { start, battle, rejoin, game } from './routes';
-import ReJoinLanding from './pages/ReJoin';
-import Game from './pages/Game';
+import { game } from './routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -23,14 +19,11 @@ const theme = createTheme(themeOptions);
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="BattlePuttHostApp">
+      <BrowserRouter basename="BattlePuttClientApp">
         <Routes>
           <Route path="/" element={<App />}>
-            <Route index element={<Navigate to={start} />} />
-            <Route path={start} element={<Landing />} />
-            <Route path={rejoin} element={<ReJoinLanding />} />
-            <Route path={`${battle}/:battleId`} element={<Battle />} />
-            <Route path={`${game}/:slug/:code`} element={<Game />} />
+            <Route index element={<Navigate to={game} />} />
+            <Route path={game} element={<Box />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,1 @@
-export const clientUrl =
-  process.env.NODE_ENV === 'development'
-    ? 'https://localhost:3000'
-    : 'https://github.com/jaboc83/BattlePuttClientApp';
+export const game = '/game';
