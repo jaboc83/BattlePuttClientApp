2d4344dd7a62ceaa097de320e3c2ec2f366fd69e
Jake Moening <jaboc83@gmail.com>
2022-12-16 16:58:15 -0600

    points

--- src/hooks/useKnockout.ts
+++ src/hooks/useKnockout.ts
@@ -1,4 +1,4 @@
-import { fetchKnockout, updateKnockout } from '../api';
+import { Knockout, Player, fetchKnockout, updateKnockout } from '../api';
 
 export const useKnockout = () => {
   return {
@@ -18,6 +18,32 @@ export const useKnockout = () => {
       currentMatch.currentPlayer =
         currentMatch.players[randomPlayerIndex].playerUsername;
       return updateKnockout(currentMatch);
+    },
+    completeTurn: (points: number, knockout: Knockout) => {
+      const addedPoints = points * (knockout?.distance || 1);
+      knockout.players.forEach(p => {
+        if (
+          p.playerUsername?.toLowerCase() ===
+          knockout.currentPlayer?.toLowerCase()
+        ) {
+          p.score = (p.score || 0) + addedPoints;
+        }
+      });
+      console.log('currentPlayer: ' + knockout.currentPlayer);
+      const nextPlayerIndex =
+        knockout.players.findIndex(
+          p =>
+            p.playerUsername?.toLowerCase() ===
+            knockout.currentPlayer?.toLowerCase(),
+        ) + 1;
+      console.log('nextPlayerIndex: ' + nextPlayerIndex);
+      const lastPlayerIndex = knockout.players.length - 1;
+      console.log('lastPlayerIndex: ' + lastPlayerIndex);
+      knockout.currentPlayer =
+        nextPlayerIndex > lastPlayerIndex
+          ? knockout.players[0].playerUsername
+          : knockout.players[nextPlayerIndex].playerUsername;
+      return updateKnockout(knockout);
     },
   };
 };
--- src/pages/Knockout/CurrentPlayerScreen.tsx
+++ src/pages/Knockout/CurrentPlayerScreen.tsx
@@ -1,6 +1,7 @@
 import { Button, Grid, Typography } from '@mui/material';
 import * as React from 'react';
 import { Game, Knockout } from '../../api';
+import { useKnockout } from '../../hooks';
 
 interface CurrentPlayerScreenProps {
   knockout: Knockout;
@@ -11,30 +12,45 @@ const CurrentPlayerScreen: React.FC<CurrentPlayerScreenProps> = ({
   knockout,
   game,
 }) => {
+  const { completeTurn } = useKnockout();
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
+
   return (
     <>
       <Typography variant="h4" align="center" gutterBottom>
         {`${knockout?.distance}' ${game.name}`}
       </Typography>
-      <Typography variant="h6" align="center" gutterBottom>
-        How many putts did you make this round?
-      </Typography>
-      <Grid container spacing={2}>
-        {Array.from(Array((knockout.numberOfDiscs || 5) + 1).keys())
-          .map(x => x)
-          .map(g => (
-            <Grid key={g} item sm={6} xs={6}>
-              <Button
-                variant="contained"
-                size="large"
-                fullWidth={true}
-                sx={{ height: '5rem', fontSize: 28, fontWeight: 600 }}
-              >
-                {g}
-              </Button>
-            </Grid>
-          ))}
-      </Grid>
+      {isSubmitting ? (
+        <Typography variant="h5" color="primary" align="center">
+          Submitting...
+        </Typography>
+      ) : (
+        <>
+          <Typography variant="h6" align="center" gutterBottom>
+            How many putts did you make this round?
+          </Typography>
+          <Grid container spacing={2}>
+            {Array.from(Array((knockout.numberOfDiscs || 5) + 1).keys())
+              .map(x => x)
+              .map(g => (
+                <Grid key={g} item sm={6} xs={6}>
+                  <Button
+                    variant="contained"
+                    size="large"
+                    fullWidth={true}
+                    sx={{ height: '5rem', fontSize: 28, fontWeight: 600 }}
+                    onClick={() => {
+                      completeTurn(g, knockout);
+                      setIsSubmitting(true);
+                    }}
+                  >
+                    {g}
+                  </Button>
+                </Grid>
+              ))}
+          </Grid>
+        </>
+      )}
     </>
   );
 };
